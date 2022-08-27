19ecec70c7e5b3b48ae257b4449f041c718e8028
OakBricks <andycruzfranco@gmail.com>
2022-08-26 23:38:18 -0500

    made `test.c` test out other logging features

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include <oaklib/oaklib.h>
 
 int main(void) {
-    log_err("sex");
+    log_err("error");
+    log_debug("debug");
+    log_info("info");
+    log_warn("warning");
     return 0;
 }
